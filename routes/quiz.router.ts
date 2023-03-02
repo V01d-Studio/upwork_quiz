194ae443bbb6a7865b3ff8f1b44dcbc75d3d1b33
sihan-tawsik <sihantawsik@gmail.com>
2023-03-02 20:11:04 +0600

    Updated quiz routers

--- routes/quiz.router.ts
+++ routes/quiz.router.ts
@@ -9,7 +9,7 @@ import { methodNotAllowed } from "../utils/functions";
 const router = express.Router();
 
 router.route("/").post(createQuiz).all(methodNotAllowed);
-router.route("/:id").get(getQuiz).all(methodNotAllowed);
 router.route("/submit-quiz").post(submitQuiz).all(methodNotAllowed);
+router.route("/:id").get(getQuiz).all(methodNotAllowed);
 
 export default router;
